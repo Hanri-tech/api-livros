06227db84b1c822efedbf67bf6917c22c10d845a
santi <hanri.ferreira@sankhya.com.br>
2024-11-25 00:09:20 -0300

    criado a nova rota de fazer o upload de arquivo no post e implementado a logica para upar arquivo no servior e vincular no banco o arquivo.

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,6 @@
 import {buscarTodosPosts, buscarPorIdPosts, savePost} from "../models/postsModel.js"
+import fs from "fs";
+import path from "path";
 
 export async function getAllPost(req, res) {
     const postsAll = await buscarTodosPosts();
@@ -15,6 +17,28 @@ export async function createPost(req, res) {
     console.log(reqBody)
     try{
         const response = await savePost(reqBody);
+        res.status(201).json(response);
+    }catch(exceptions){
+        console.log(`[LOG-ERROR] - Erro ao tentar inserir registro no banco de dados... STACKTRACE: ${exceptions}`);
+        res.status(500).json({error: "Falha Interna ao inserir registro no banco de dados..."})
+    }
+
+}
+
+export async function uploadImg(req, res) {
+    const reqBody = {
+        descricao: null,
+        imgUrl: req.file.originalname,
+        alt: null
+    }
+
+    console.log(reqBody)
+    try{
+        const response = await savePost(reqBody);
+
+        const newPathImg = `uploads/${response.insertedId}.${path.extname(req.file.originalname)}`
+        fs.renameSync(req.file.path, newPathImg)
+        console.log(req.file);
         res.status(201).json(response);
     }catch(exceptions){
         console.log(`[LOG-ERROR] - Erro ao tentar inserir registro no banco de dados... STACKTRACE: ${exceptions}`);
--- src/routes/postsRouters.js
+++ src/routes/postsRouters.js
@@ -1,5 +1,21 @@
 import express from "express"
-import { getAllPost, getByIdPosts, createPost } from "../controllers/postsController.js";
+import { getAllPost, getByIdPosts, createPost, uploadImg } from "../controllers/postsController.js";
+import multer from "multer";
+import path from "path";
+
+// Essa Logica de salvar arquivo precisa ser escrita apenas na maquina WINDOWS.
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // Define a pasta onde os arquivos serão armazenados
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        // Define o nome do arquivo (mantendo a extensão original)
+        cb(null, `${file.fieldname}${path.extname(file.originalname)}`);
+    },
+});
+
+const upload = multer({dest : "./uploads", storage})
 
 const routers = (app) => {
     app.use(express.json());
@@ -9,6 +25,8 @@ const routers = (app) => {
     app.get("/posts/:id", getByIdPosts);
 
     app.post("/posts", createPost);
+
+    app.post("/upload", upload.single("imagem"), uploadImg);
 }
 
 export default routers;
